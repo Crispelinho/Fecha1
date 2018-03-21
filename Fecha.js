9b472e3c6c3993b84bddec62d43d6bb897938656
Estudiante <CristhianC>
2018-03-20 19:17:49 -0500

    version:0.4

--- Fecha.js
+++ Fecha.js
@@ -10,7 +10,7 @@ var fecha =
     public Fecha() {
     }
 
-    int cars = [31,28,31,];
+    int meses = [31,28,31,30,31,30,31,31,30,31,30,31];
 
     //Constructor con parámetros
     public Fecha(int dia, int mes, int año) {
@@ -39,6 +39,35 @@ var fecha =
     }
     public int getAño() {
         return año;
+    }
+
+    public boolean fechaCorrecta() {
+        boolean diaCorrecto, mesCorrecto, añoCorrecto;
+        añoCorrecto = año > 0;
+        mesCorrecto = mes >= 1 && mes <= 12;
+        switch (mes) {
+        	case 1: diaCorrecto = dia >= 1 && dia <= 31;
+            case 2:
+                if (esBisiesto()) {
+                    diaCorrecto = dia >= 1 && dia <= 29;
+                } else {
+                    diaCorrecto = dia >= 1 && dia <= 28;
+                }
+                break;
+            case 3: diaCorrecto = dia >= 1 && dia <= 31; break;
+            case 4: diaCorrecto = dia >= 1 && dia <= 30; break;
+            case 5: diaCorrecto = dia >= 1 && dia <= 31; break;                       
+            case 6: diaCorrecto = dia >= 1 && dia <= 30; break;
+            case 7: diaCorrecto = dia >= 1 && dia <= 31; break;
+            case 8: diaCorrecto = dia >= 1 && dia <= 31; break;                      
+            case 9: diaCorrecto = dia >= 1 && dia <= 30; break;
+            case 10: diaCorrecto = dia >= 1 && dia <= 31; break;          
+            case 11: diaCorrecto = dia >= 1 && dia <= 30; break;
+            case 12: diaCorrecto = dia >= 1 && dia <= 31; break;     
+            default: diaCorrecto = dia >= 1 && dia <= 31; break;         
+        }
+        
+        return diaCorrecto && mesCorrecto && añoCorrecto;
     }
 
     private boolean esBisiesto() {
